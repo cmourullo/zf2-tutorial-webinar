719a80f30bd98381921754429f5b9add2278489c
Carlos Mourullo <carlosmourullo@gmail.com>
2016-08-20 11:47:43 +0100

    add neccesary modules

--- config/application.config.php
+++ config/application.config.php
@@ -2,7 +2,10 @@
 return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
+        'ZendDeveloperTools',
         'Application',
+        'DoctrineModule',
+        'DoctrineORMModule',
     ),
 
     // These are various options for the listeners attached to the ModuleManager
